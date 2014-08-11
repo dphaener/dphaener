a0adc42728f21247404f406f3bd116a168ac01c8
Darin Haener <dphaener@gmail.com>
2014-08-11 12:09:53 -0700

    Changed ruby version

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.1.1'
+ruby '2.1.2'
 
 gem 'bcrypt-ruby', :require => 'bcrypt'
 gem 'bootstrap-sass', '~> 3.2'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,21 +1,7 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
+set :rails_env, "production"
+set :branch, "master"
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
-
-server 'dphaener.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '192.241.227.64', user: "app", roles: %w{web app}
 
 
 # Custom SSH Options
